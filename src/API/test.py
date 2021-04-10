91025d24da52c305071143d332fcdef2cb2a9afa
momciloknezevic7 <moma.knezevic7@gmail.com>
2021-04-10 18:38:00 +0200

    Scrap file modifed

--- src/API/test.py
+++ src/API/test.py
@@ -5,13 +5,16 @@ import requests
 from urllib.parse import urlsplit
 from collections import deque
 from bs4 import BeautifulSoup
+import bs4
 import pandas as pd
+import string
 
 
 def scrapSite(urls, searchInput):
     unscraped = deque(urls)
     scraped = set()
 
+    dangerIndex = 0
     while len(unscraped):
         url = unscraped.popleft()
         scraped.add(url)
@@ -26,14 +29,23 @@ def scrapSite(urls, searchInput):
             print("Unsuccessful request!")
             continue
 
-        r = re.compile(rf"{searchInput}|(\bvanredno stanje\b)|(\bpožar\b)|(\bzemljotres\b)|(\bpoplava\b)|(\brat\b)|(\bepidemija\b)|(\bpandemija\b)|(\bprirodna katastrofa\b)", flags= re.I | re.X)
-        requiredWords = r.findall(response.text)
+        soup = bs4.BeautifulSoup(response.text, 'html.parser')
 
-        print("\n----->URL: ", url)
-        for x in requiredWords:
-            print(x)
+        #rb = re.compile(
+        #    rf"{searchInput}|(\bvanredno stanje\b)|(\bpožar\b)|(\bzemljotres\b)|(\bpoplava\b)|(\brat\b)|(\bepidemija\b)|(\bpandemija\b)|(\bprirodna katastrofa\b)",
+        #    flags=re.I | re.X)
+        # r = re.compile(rf"^.*?{searchInput}.*?\bvanredno stanje\b.*?\bpožar\b.*?\bzemljotres\b.*?\bpoplava\b.*?\brat\b.*?\bepidemija\b.*?\bpandemija\b.*?\bprirodna katastrofa\b.*?$", flags= re.I | re.X)
+        # r = re.compile(rf".*?{searchInput}.*?(\bvanredno stanje\b).*?", flags= re.I | re.X)
+        r = re.compile(
+            rf"{searchInput}", flags=re.I | re.X)
+        results = soup.body.find_all(string=r, recursive=True)
 
+        print(results)
 
+        #requiredWords = r.findall(response.text)
+        #print("\n----->URL: ", url)
+        #for x in requiredWords:
+        #    print(x)
 
 
 
@@ -50,9 +62,17 @@ def googleSearch(searchInput):
     '''
 
     searchResult = [
+        #----------------------------------------------------------Portali
         'https://www.blic.rs/vanredno-stanje',
         'https://rs.n1info.com/tag/vanredno-stanje/',
         'https://nova.rs/tag/vanredno-stanje/'
+        'https://www.juznevesti.com/Tagovi/vanredno-stanje.sr.html'
+        
+        #----------------------------------------------------------Regioni
+        'https://akv.org.rs/category/vanredno-stanje/?script=lat',
+        #'https://www.juznevesti.com/Tagovi/vanredno-stanje.sr.html',
+        'http://www.rtvsumadija.com/tag/vanredno-stanje/'
+
     ]
 
     #           links         cityName
@@ -70,7 +90,15 @@ if __name__ == "__main__":
 
     argv = sys.argv
     #cityName = argv[1:]
-    cityName = "Beograd"
-    searchInput = "" + cityName + " "
+    #cityName = "Pančevo"
+    #cityName = "Beograd"
+    #cityName = "Novi Sad"
+    #cityName = "Niš"
+    #cityName = "Vranje"
+    cityName = "Kragujev"
+    cityName = "Jagodin"
+    #cityName = "Kruševac"
+
+    searchInput = "" + cityName
 
     googleSearch(searchInput)
